3cd9ba70205f1348fa6e6f46e75d65b3dcdcfe6f
pwa <peter.waegemann@googlemail.com>
2012-12-01 14:22:21 +0100

    returning street to client

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 sudo apt-get install python-virtualenv
 sudo apt-get install python-pip
 
+
 virtualenv venv
+
+
 source venv/bin/activate
 pip install django
 
+python manage.py runserver
 
 
--- app/views.py
+++ app/views.py
@@ -1,5 +1,20 @@
 from django.http import HttpResponse
+import urllib
+import json
 
 def post(request):
     print request.POST['lat'], request.POST['lon']
-    return HttpResponse()
+
+    street = getStreet(request.POST['lat'], request.POST['lon'])
+
+    res =  street["results"][0]["address_components"][0]["short_name"]
+    print res
+    
+    return HttpResponse(content=res)
+
+
+def getStreet(lat,lon):
+    url = "http://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon+"&sensor=true"
+    filehandle = urllib.urlopen( url )
+    dic = json.load( filehandle )
+    return dic
